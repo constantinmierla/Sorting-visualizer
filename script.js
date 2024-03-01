a86025883d2d66e31382c8a0e197772a292897e5
Dexter <costi.mierla284@gmail.com>
2024-03-01 22:44:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -11,13 +11,16 @@ const margin = 30;
 const availableWidth = myCanvas.width - 2*margin;
 const spacing = availableWidth/ n;
 
-const colors = ["red","blue","green","yellow","purple","orange","pink","brown","black","white","gray","cyan","magenta","lime"];
+const colors = ['red','green','blue','yellow','purple','orange','pink','black','white','gray','brown','cyan','magenta','lime','indigo','teal','violet',
+                'navy','maroon','olive','aqua','fuchsia', 'silver','lime','crimson','coral','gold','plum','ivory','azure','lavender','salmon','tan',
+                'khaki','wheat','beige','mint','apricot','cerulean','rose'];
 
 const sockColors = [];
 
 for (let i = 0; i<n/2; i++) {
     const t = i/(n/2-1);
-    
+    sockColors.push(colors[i]);
+    sockColors.push(colors[i]);
     array.push(lerp(0.2,1,t));
     array.push(lerp(0.2,1,t));
 }
@@ -25,12 +28,13 @@ for (let i = 0; i<n/2; i++) {
 for (let i = 0; i < array.length; i++){
     const j = Math.floor(Math.random()*array.length);
     [array[i], array[j]] = [array[j], array[i]];
+    [sockColors[i], sockColors[j]] = [sockColors[j], sockColors[i]];
 }
 for (let i = 0; i < array.length; i++){
     const x = i*spacing+spacing/2 + margin;
     const y = stringHeight;
     const height = 0.4*myCanvas.height*array[i];
-    socks[i] = new Sock(x,y,height);
+    socks[i] = new Sock(x,y,height,sockColors[i]);
 }
 
 const moves = bubbleSort(array);
